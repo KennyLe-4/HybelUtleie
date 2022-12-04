203b87d36bcdb3fd7af8a96a2d66eba270492092
Kiwinas <kevinas@lyse.net>
2022-12-04 16:42:40 +0100

    kommentar og litt fjerning av unødvendig kode

--- www/registrerBruker.php
+++ www/registrerBruker.php
@@ -5,14 +5,15 @@ $sql = "INSERT INTO bruker
         (epost, fnavn, enavn, passord) 
         VALUES 
         (:epost, :fnavn, :enavn, :passord)";
-
+//query for å legge til i databasen og setter verdi på de forrskellige
 $q = $pdo->prepare($sql);
-
+//forbedre queryen
 
 $q->bindParam(':epost', $epost, PDO::PARAM_STR);
 $q->bindParam(':fnavn', $firstname, PDO::PARAM_STR);
 $q->bindParam(':enavn', $lastname, PDO::PARAM_STR);
 $q->bindParam(':passord', $passord, PDO::PARAM_STR);
+//binder de forskjellige verdiene fra databsen med php parametere
 
 
 include_once('../Includes/VaskingAvTagger.inc.php'); // Henter vaskingAvTagger funksjonen. 
@@ -42,6 +43,7 @@ if (isset($_REQUEST['registrer'])) {
     } else {
         echo "Registrerigen ble <b>ikke</b> gjennomført";
 			header('Location: registrerbruker.php');
+			//hvis ikke for brukeren feil medling
 		exit();
 
 
@@ -90,91 +92,8 @@ if (isset($_REQUEST['registrer'])) {
                     <div class="text-center"><a class="small" href="logginn.php">Har du allerde en bruker? Logg inn!</a></div>
                 </div>
             </div>
-        </div><script>
+        </div>
 
-
-
-
-
-
-/*
-
-	let email = document.getElementById("email")
-	let password = document.getElementById("password")
-	let verifyPassword = document.getElementById("verifyPassword")
-	let submitBtn = document.getElementById("submitBtn")
-	let emailErrorMsg = document.getElementById('emailErrorMsg')
-	let passwordErrorMsg = document.getElementById('passwordErrorMsg')
-
-	function displayErrorMsg(type, msg) {
-		if(type == "email") {
-			emailErrorMsg.style.display = "block"
-			emailErrorMsg.innerHTML = msg
-			submitBtn.disabled = true
-		}
-		else {
-			passwordErrorMsg.style.display = "block"
-			passwordErrorMsg.innerHTML = msg
-			submitBtn.disabled = true
-		}
-	}
-
-	function hideErrorMsg(type) {
-		if(type == "email") {
-			emailErrorMsg.style.display = "none"
-			emailErrorMsg.innerHTML = ""
-			submitBtn.disabled = true
-			if(passwordErrorMsg.innerHTML == "")
-				submitBtn.disabled = false
-		}
-		else {
-			passwordErrorMsg.style.display = "none"
-			passwordErrorMsg.innerHTML = ""
-			if(emailErrorMsg.innerHTML == "")
-				submitBtn.disabled = false
-		}
-	}
-	
-	// Validate password upon change
-	password.addEventListener("change", function() {
-
-		// If password has no value, then it won't be changed and no error will be displayed
-		if(password.value.length == 0 && verifyPassword.value.length == 0) hideErrorMsg("password")
-		
-		// If password has a value, then it will be checked. In this case the passwords don't match
-		else if(password.value !== verifyPassword.value) displayErrorMsg("password", "Passwords do not match")
-		
-		// When the passwords match, we check the length
-		else {
-			// Check if the password has 8 characters or more
-			if(password.value.length >= 8)
-				hideErrorMsg("password")
-			else
-				displayErrorMsg("password", "Password must be at least 8 characters long")
-		}
-	})
-	
-	verifyPassword.addEventListener("change", function() {
-		if(password.value !== verifyPassword.value)
-			displayErrorMsg("password", "Passwords do not match")
-		else {
-			// Check if the password has 8 characters or more
-			if(password.value.length >= 8)
-				hideErrorMsg("password")
-			else
-				displayErrorMsg("password", "Password must be at least 8 characters long")
-		}
-	})
-
-	// Validate email upon change
-	email.addEventListener("change", function() {
-		// Check if the email is valid using a regular expression (string@string.string)
-		if(email.value.match(/^[^@]+@[^@]+\.[^@]+$/))
-			hideErrorMsg("email")
-		else
-			displayErrorMsg("email", "Invalid email")
-	});*/
-</script>
     </div>
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
     <script src="assets/js/bs-init.js"></script>
