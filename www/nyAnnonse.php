1837fb2a9c56882cf8739a38fc46d328f95a3b85
Kenny Le <79541722+KennyLe-4@users.noreply.github.com>
2022-12-05 16:25:27 +0100

    ny mal til annonse

--- www/nyAnnonse.php
+++ www/nyAnnonse.php
@@ -110,69 +110,22 @@ if (isset($_REQUEST['opprettAnnonse'])) {
         // "Data were not inserted into database.";
     }
 }
-
-/*
-    try {
-        $q->execute();
-    } catch (PDOException $e) {
-        echo "Error querying database: " . $e->getMessage() . "<br>"; // Never do this in production
-    }
-    //$q->debugDumpParams();
-    //Sjekker om noe er satt inn, returnerer UID. I dette tilfelle, redirecter til hjem.php
-    if ($pdo->lastInsertId() > 0) {
-            $_SESSION['meldinger'] = "Din annonse er lagt til";
-            header('Location: hjemmeside.php'); // blir sendt tilbake til hjemmesiden med suksessfull melding 
-            
-    } else {
-        $_SESSION['feilmeldinger'] = "Det var en feil med din opplastning";
-        header('Location: hjemmeside.php'); // blir værende, men får feilkode
-        
-    }
-}
-*/
-
-
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-<meta charset="utf-8">
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>hybel</title>
-    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../assets/css/aos.min.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/6.4.8/swiper-bundle.min.css">
-    <link rel="stylesheet" href="../assets/css/Navbar-Right-Links-icons.css">
-    <link rel="stylesheet" href="../assets/css/Projects-Grid-images.css">
-    <link rel="stylesheet" href="../assets/css/Simple-Slider-Simple-Slider.css">
+    <link rel="stylesheet" href="assets/css/Navbar-Right-Links-icons.css">
+    <link rel="stylesheet" href="assets/css/Projects-Grid-images.css">
+    <link rel="stylesheet" href="assets/css/Simple-Slider-Simple-Slider.css">
 </head>
-<body>
-<?php 
-    
-    if(isset($_SESSION['feilmeldinger']))
-    {
-        ?>
-            <div class="alert alert-warning alert-dismissible fade show" role="alert">
-                <strong>Det var en feil med opplastningen!</strong> <?= $_SESSION['meldinger']; ?>
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-            </div>
-        <?php 
-        unset($_SESSION['meldinger']);
-    } 
 
-?>
-<nav class="navbar navbar-light navbar-expand-md py-3" data-aos="fade">
-        <div class="container"><a class="navbar-brand d-flex align-items-center" href="hjemmeside.php"><span class="bs-icon-sm bs-icon-rounded bs-icon-primary d-flex justify-content-center align-items-center me-2 bs-icon"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-house">
-                        <path fill-rule="evenodd" d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"></path>
-                        <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"></path>
-                    </svg></span><span>Hybel</span></a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-2"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
-        </div>
-    </nav>
-    <div class="container py-4 py-xl-5">
-        <div class="row mb-5">
-            <div class="col-md-40 col-xl-6 text-center mx-auto">
+<body>
     <div class="simple-slider">
         <div class="swiper-container">
             <div class="swiper-pagination"></div>
@@ -180,28 +133,21 @@ if (isset($_REQUEST['opprettAnnonse'])) {
             <div class="swiper-button-next"></div>
         </div>
     </div>
-    <section class="position-relative py-4 py-xl-5">
-        <div class="container position-relative">
-            <div class="row d-flex justify-content-center">
-                <div class="col-md-8 col-lg-6 col-xl-5 col-xxl-4">
-                    <div class="card mb-5">
-                        <div class="card-body p-sm-5">
-                            <h2 class="text-center mb-4">Ny annonse</h2>
+    <div class="container" style="position:absolute; left:0; right:0; top: 50%; transform: translateY(-50%); -ms-transform: translateY(-50%); -moz-transform: translateY(-50%); -webkit-transform: translateY(-50%); -o-transform: translateY(-50%);">
+        <div class="row d-flex d-xl-flex justify-content-center justify-content-xl-center">
+            <div class="col-sm-12 col-lg-10 col-xl-9 col-xxl-7 bg-white shadow-lg" style="border-radius: 5px;">
+                <div class="p-5">
+				<h2 class="text-center mb-4">Ny annonse</h2>
                             <form method="post" enctype="multipart/form-data" >
-                          
+                          	   <!-- Dette er insert feltene for overskrift, adresse, pris, depositum og beskrivelse-->
                                 <div class="mb-3"><input class="form-control" type="text" id="name-1" name="overskrift" placeholder="Overskrift" required></div> 
-
                                 <div class="mb-3"><input class="form-control" type="text" id="email-1" name="gateAdresse" placeholder="Adresse" required></div>
-
                                 <div class="mb-3"><input class="form-control" type="number" id="email-1" name="pris" placeholder="Pris" required></div>
-
                                 <div class="mb-3"><input class="form-control" type="number" id="email-1" name="depositum" placeholder="Depositum" required></div>
-
-
-                                <div class="mb-3"><textarea class="form-control" id="message-2" name="beskrivelse" rows="6" placeholder="Beskrivelse" required></textarea></div>
+                                <div class="mb-3"><textarea class="form-control" id="message-2" name="beskrivelse" rows="3" placeholder="Beskrivelse, maks " required></textarea></div>
 
                                 <!-- De ulike knappene for type bolig  -->
-                                            <label>Hva slags type bolig skal du leie ut<label>
+								<div class="mb-3"><label>Hva slags type bolig skal du leie ut<label></div>
                                             <select class="form-select" name="boligType" aria-label="Default select example" required>
                                                 <option selected>Åpne denne menyen</option>
                                                 <option value="Bofelleskap">Rom i bofelleskap</option>
@@ -210,10 +156,10 @@ if (isset($_REQUEST['opprettAnnonse'])) {
                                                 <option value="Leilighet">Leilighet</option>
                                                 <option value="Hus">Hus</option>
                                                 <option value="Annet">Annet</option>
-
                                             </select><br>
 
-                                            <label>Antall etasjer<label>
+                                            <!-- Dette er antall rom option knapp -->
+                                            <div class="mb-3"><label>Antall etasjer<label></div>
                                             <select class="form-select" name="boligEtasje" aria-label="Default select example" required>
                                                 <option selected>Åpne denne menyen</option>
                                                 <option value="1">1</option>
@@ -222,8 +168,8 @@ if (isset($_REQUEST['opprettAnnonse'])) {
                                                 <option value="4">4</option>
                                                 <option value="Annet">Annet</option>
                                             </select><br>
-
-                                            <label>Antall rom<label>
+											<!-- Dette er antall rom option knapp -->
+											<div class="mb-3"><label>Antall rom<label></div>
                                             <select class="form-select" name="antallRom" aria-label="Default select example" required>
                                                 <option selected>Åpne denne menyen</option>
                                                 <option value="1">1</option>
@@ -233,12 +179,9 @@ if (isset($_REQUEST['opprettAnnonse'])) {
                                                 <option value="Annet">Annet</option>
                                             </select><br>
                                         
-                                            <div class="mb-3"><input name="upload-file" type="file" required><br></div>
+											<div class="mb-3"><input name="upload-file" type="file" required><br></div>
 
-
-                        
-                            
-                                      
+                                       <!-- Dette er legg til fil knappen -->
                                         <div><button class="btn btn-primary d-block w-100" name="opprettAnnonse" type="submit">Opprett annonse</button></div>
                             </form>
                         </div>
@@ -247,6 +190,7 @@ if (isset($_REQUEST['opprettAnnonse'])) {
             </div>
         </div>
     </section>
+                    
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/6.4.8/swiper-bundle.min.js"></script>
     <script src="assets/js/Simple-Slider.js"></script>
