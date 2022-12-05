45637eada1f34b46eb051e026de82d1eee516fa7
Kenny Le <79541722+KennyLe-4@users.noreply.github.com>
2022-12-05 15:36:19 +0100

    sjekke

--- www/hjemmeside.php
+++ www/hjemmeside.php
@@ -194,12 +194,6 @@ require_once ('../Includes/db.inc.php');
                             <div class="carousel-item"><img class="w-100 d-block" src="https://cdn.bootstrapstudio.io/placeholders/1400x800.png" alt="Slide Image"></div>
                             <div class="carousel-item"><img class="w-100 d-block" src="https://cdn.bootstrapstudio.io/placeholders/1400x800.png" alt="Slide Image"></div>
                         </div>
-                        <div><a class="carousel-control-prev" href="#carousel-1" role="button" data-bs-slide="prev"><span class="visually-hidden">Previous</span></a><a class="carousel-control-next" href="#carousel-1" role="button" data-bs-slide="next"><span class="carousel-control-next-icon"></span><span class="visually-hidden">Next</span></a></div>
-                        <ol class="carousel-indicators">
-                            <li data-bs-target="#carousel-1" data-bs-slide-to="0" class="active"></li>
-                            <li data-bs-target="#carousel-1" data-bs-slide-to="1"></li>
-                            <li data-bs-target="#carousel-1" data-bs-slide-to="2"></li>
-                        </ol>
                     </div><button class="btn btn-primary" type="button">Se hybel</button>
                 </div>
             </div>
