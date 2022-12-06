7a699fce0944b8b4e9caff9ea9c095c1114cfa2a
Kiwinas <kevinas@lyse.net>
2022-12-06 13:15:13 +0100

    Slettet unødvendige bilder og endret litt kode

--- www/listeAnnonser.php
+++ www/listeAnnonser.php
@@ -2,7 +2,7 @@
 require_once('/Applications/XAMPP/xamppfiles/htdocs/HybelUtleie/Includes/VaskingAvTagger.inc.php'); ?>
 
 
-<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css%22%3E
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
 <div class="container">
 <div class="card-body" >
 <h1>Annonser</h1>
