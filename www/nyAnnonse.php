226d5693cc3f7f5b3f9148558b99c85a3dc0a113
Kenny Le <79541722+KennyLe-4@users.noreply.github.com>
2022-12-06 17:48:38 +0100

    med kevin

--- www/nyAnnonse.php
+++ www/nyAnnonse.php
@@ -123,14 +123,13 @@ if (isset($_POST['opprettAnnonse'])) {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>hybel</title>
-    <link rel="stylesheet" href="/Applications/XAMPP/xamppfiles/htdocs/HybelUtleie/assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/6.4.8/swiper-bundle.min.css">
-    <link rel="stylesheet" href="/Applications/XAMPP/xamppfiles/htdocs/HybelUtleie/assets/css/Navbar-Right-Links-icons.css">
-    <link rel="stylesheet" href="/Applications/XAMPP/xamppfiles/htdocs/HybelUtleie/assets/css/Projects-Grid-images.css">
-    <link rel="stylesheet" href="/Applications/XAMPP/xamppfiles/htdocs/HybelUtleie/assets/css/Simple-Slider-Simple-Slider.css">
+    <link rel="stylesheet" href=../assets/css/Navbar-Right-Links-icons.css>
+    <link rel="stylesheet" href="../assets/css/Projects-Grid-images.css">
+    <link rel="stylesheet" href="../assets/css/Simple-Slider-Simple-Slider.css">
+    <title>Document</title>
 </head>
-
 <body>
     <div class="simple-slider">
         <div class="swiper-container">
@@ -143,7 +142,7 @@ if (isset($_POST['opprettAnnonse'])) {
     <!-- Dette er hybel logo - sender deg til hjemmeside -->
     <body>
     <nav class="navbar navbar-light navbar-expand-md py-3" data-aos="fade">
-        <div class="container"><a class="navbar-brand d-flex align-items-center" href="../hjemmeside.php"><span class="bs-icon-sm bs-icon-rounded bs-icon-primary d-flex justify-content-center align-items-center me-2 bs-icon"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-house">
+        <div class="container"><a class="navbar-brand d-flex align-items-center" href="hjemmeside.php"><span class="bs-icon-sm bs-icon-rounded bs-icon-primary d-flex justify-content-center align-items-center me-2 bs-icon"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-house">
                         <path fill-rule="evenodd" d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"></path>
                         <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"></path>
                     </svg></span><span>Hybel</span></a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-2"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
